4f5988ed3d508770b779a7019b786cb7b9f8650f
Samuel Kashul <samuelkashul@gmail.com>
2026-02-12 14:33:02 -0500

    Add audio recording functionality to script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+let mediaRecorder;
+let audioChunks = [];
+
+const recordBtn = document.getElementById("recordBtn");
+const stopBtn = document.getElementById("stopBtn");
+
+recordBtn.onclick = async () => {
+  const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+  mediaRecorder = new MediaRecorder(stream);
+
+  mediaRecorder.ondataavailable = event => {
+    audioChunks.push(event.data);
+  };
+
+  mediaRecorder.onstop = () => {
+    const audioBlob = new Blob(audioChunks, { type: "audio/wav" });
+    audioChunks = [];
+    console.log("Recording finished:", audioBlob);
+    alert("Recording complete! (Not uploaded yet)");
+  };
+
+  mediaRecorder.start();
+  alert("Recording started");
+};
+
+stopBtn.onclick = () => {
+  if (mediaRecorder) {
+    mediaRecorder.stop();
+  }
+};
 
